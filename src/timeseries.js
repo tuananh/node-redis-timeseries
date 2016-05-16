b0beec22b716fea3953739e00b86c1d3fce7b96b
Tuan Anh Tran <me@tuananh.org>
2016-05-16 10:35:21 +0700

    clean up code, indentation, etc...

--- src/timeseries.js
+++ src/timeseries.js
@@ -1,25 +1,25 @@
-
 var TimeSeries = module.exports = function(redis, keyBase, granularities) {
-  this.redis = redis;
-  this.keyBase = keyBase || 'stats';
-  this.pendingMulti = redis.multi();
-  this.granularities = granularities || {
-    '1second'  : { ttl: this.minutes(5), duration: 1 },
-    '1minute'  : { ttl: this.hours(1)  , duration: this.minutes(1) },
-    '5minutes' : { ttl: this.days(1)   , duration: this.minutes(5) },
-    '10minutes': { ttl: this.days(1)   , duration: this.minutes(10) },
-    '1hour'    : { ttl: this.days(7)   , duration: this.hours(1) },
-    '1day'     : { ttl: this.weeks(52) , duration: this.days(1) }
-  };
-};
+    this.redis = redis;
+    this.keyBase = keyBase || 'stats';
+    this.pendingMulti = redis.multi();
+    this.granularities = granularities || {
+        '1second': { ttl: this.minutes(5), duration: 1 },
+        '1minute': { ttl: this.hours(1), duration: this.minutes(1) },
+        '5minutes': { ttl: this.days(1), duration: this.minutes(5) },
+        '10minutes': { ttl: this.days(1), duration: this.minutes(10) },
+        '1hour': { ttl: this.days(7), duration: this.hours(1) },
+        '1day': { ttl: this.weeks(52), duration: this.days(1) }
+    }
+}
+
 
 /** Helper functions that compute various durations in secs */
-TimeSeries.prototype.seconds = function(i) { return i; };
-TimeSeries.prototype.minutes = function(i) { return i*60; };
-TimeSeries.prototype.hours   = function(i) { return i*this.minutes(60); };
-TimeSeries.prototype.days    = function(i) { return i*this.hours(24); };
-TimeSeries.prototype.weeks   = function(i) { return i*this.days(7); };
-TimeSeries.prototype.months  = function(i) { return i*(this.weeks(4)+this.days(2)); };
+TimeSeries.prototype.seconds = function(i) { return i }
+TimeSeries.prototype.minutes = function(i) { return i*60 }
+TimeSeries.prototype.hours   = function(i) { return i*this.minutes(60) }
+TimeSeries.prototype.days    = function(i) { return i*this.hours(24) }
+TimeSeries.prototype.weeks   = function(i) { return i*this.days(7) }
+TimeSeries.prototype.months  = function(i) { return i*(this.weeks(4)+this.days(2)) }
 
 /**
  * Record a hit for the specified stats key
@@ -35,20 +35,21 @@ TimeSeries.prototype.months  = function(i) { return i*(this.weeks(4)+this.days(2
  * `increment` should be an integer, and defaults to 1
  */
 TimeSeries.prototype.recordHit = function(key, timestamp, increment) {
-  var self = this;
+    var self = this
 
-  Object.keys(this.granularities).forEach(function(gran) {
-    var properties = self.granularities[gran],
-        keyTimestamp = getRoundedTime(properties.ttl, timestamp),
-        tmpKey = [self.keyBase, key, gran, keyTimestamp].join(':'),
-        hitTimestamp = getRoundedTime(properties.duration, timestamp);
+    Object.keys(this.granularities).forEach(function(gran) {
+        var properties = self.granularities[gran]
+        var keyTimestamp = getRoundedTime(properties.ttl, timestamp)
+        var tmpKey = [self.keyBase, key, gran, keyTimestamp].join(':')
+        var hitTimestamp = getRoundedTime(properties.duration, timestamp)
 
-   self.pendingMulti.hincrbyfloat(tmpKey, hitTimestamp, increment || 1);
-   self.pendingMulti.expireat(tmpKey, keyTimestamp + 2 * properties.ttl);
-  });
+        self.pendingMulti.hincrbyfloat(tmpKey, hitTimestamp, increment ||  1)
+        self.pendingMulti.expireat(tmpKey, keyTimestamp + 2 * properties.ttl)
+    })
 
-  return this;
+    return this
 };
+
 
 /*
  *  Removing hits
@@ -66,67 +67,69 @@ TimeSeries.prototype.recordHit = function(key, timestamp, increment) {
  *    .exec();
  */
 TimeSeries.prototype.removeHit = function(key, timestamp, decrement) {
-  return this.recordHit(key, timestamp, -(decrement || 1));
-};
+    return this.recordHit(key, timestamp, -(decrement || 1))
+}
 
 /**
  * Execute the current pending redis multi
  */
 TimeSeries.prototype.exec = function(callback) {
-  // Reset pendingMulti before proceeding to
-  // avoid concurrent modifications
-  var current = this.pendingMulti;
-  this.pendingMulti = this.redis.multi();
+    // Reset pendingMulti before proceeding to
+    // avoid concurrent modifications
+    var current = this.pendingMulti
+    this.pendingMulti = this.redis.multi()
 
-  current.exec(callback);
-};
+    current.exec(callback)
+}
+
 
 /**
  * getHits("messages", "10minutes", 3, cb)
  *   --> "messages" hits during the last 3 '10minutes' chunks
  */
 TimeSeries.prototype.getHits = function(key, gran, count, callback) {
-  var properties = this.granularities[gran],
-      currentTime = getCurrentTime();
+    var properties = this.granularities[gran]
+    var currentTime = getCurrentTime()
 
-  if (typeof properties === "undefined") {
-    return callback(new Error("Unsupported granularity: "+gran));
-  }
-
-  if (count > properties.ttl / properties.duration) {
-    return callback(new Error("Count: "+count+" exceeds the maximum stored slots for granularity: "+gran));
-  }
-
-  var from = getRoundedTime(properties.duration, currentTime - count*properties.duration),
-      to = getRoundedTime(properties.duration, currentTime);
-
-  for(var ts=from, multi=this.redis.multi(); ts<=to; ts+=properties.duration) {
-    var keyTimestamp = getRoundedTime(properties.ttl, ts),
-        tmpKey = [this.keyBase, key, gran, keyTimestamp].join(':');
-
-    multi.hget(tmpKey, ts);
-  }
-
-  multi.exec(function(err, results) {
-    if (err) {
-      return callback(err);
+    if (typeof properties === "undefined")  {
+        return callback(new Error("Unsupported granularity: " + gran))
     }
 
-    for(var ts=from, i=0, data=[]; ts<=to; ts+=properties.duration, i+=1) {
-      data.push([ts, results[i] ? parseFloat(results[i]) : 0]);
+    if (count > properties.ttl / properties.duration) {
+        return callback(new Error("Count: " + count + " exceeds the maximum stored slots for granularity: " + gran))
     }
 
-    return callback(null, data.slice(Math.max(data.length - count, 0)));
-  });
-};
+    var from = getRoundedTime(properties.duration, currentTime - count * properties.duration),
+        to = getRoundedTime(properties.duration, currentTime)
+
+    for (var ts = from, multi = this.redis.multi(); ts <= to; ts += properties.duration) {
+        var keyTimestamp = getRoundedTime(properties.ttl, ts)
+        var tmpKey = [this.keyBase, key, gran, keyTimestamp].join(':')
+
+        multi.hget(tmpKey, ts)
+    }
+
+    multi.exec(function(err, results) {
+        if (err) {
+            return callback(err)
+        }
+
+        for (var ts = from, i = 0, data = []; ts <= to; ts += properties.duration, i += 1) {
+            data.push([ts, results[i] ? parseFloat(results[i]) : 0])
+        }
+
+        return callback(null, data.slice(Math.max(data.length - count, 0)))
+    })
+}
+
 
 // Get current timestamp in seconds
 var getCurrentTime = function() {
-  return Math.floor(Date.now() / 1000);
+    return Math.floor(Date.now() / 1000)
 };
 
 // Round timestamp to the 'precision' interval (in seconds)
 var getRoundedTime = function(precision, time) {
-  time = time || getCurrentTime();
-  return Math.floor(time / precision) * precision;
+    time = time || getCurrentTime()
+    return Math.floor(time / precision) * precision
 };
--- test/concurrentTest.js
+++ test/concurrentTest.js
@@ -1,46 +1,50 @@
-var async = require('async'),
-    vows = require('vows'),
-    assert = require('assert'),
-    redis = require('redis').createClient(),
-    TimeSeries = require('../');
+var async = require('async')
+var vows = require('vows')
+var assert = require('assert')
+var redis = require('redis').createClient()
+var TimeSeries = require('../')
 
 // number of concurrent requests
-var N = 15;
+var N = 15
 
 function range(n) {
-  return Array.apply(null, Array(n)).map(function (_, i) {return i;});
+    return Array.apply(null, Array(n)).map(function(_, i) {
+        return i
+    })
 }
 
 vows.describe('TimeSeries Tests').addBatch({
-  'When recording hits concurrently': {
-    topic: function() {
-      var ts = new TimeSeries(redis);
+    'When recording hits concurrently': {
+        topic: function() {
+            var ts = new TimeSeries(redis)
 
-      // Reduce granularities for easier testing
-      ts.granularities = {
-        'h' : { ttl: ts.days(7), duration: ts.hours(1) }
-      };
+            // Reduce granularities for easier testing
+            ts.granularities = {
+                'h': { ttl: ts.days(7), duration: ts.hours(1) }
+            }
 
-      // Build N concurrent hit queries
-      function handler(i) {
-        return function(callback) {
-          ts.recordHit('key_'+i).exec(callback);
-        };
-      }
-      
-      // Select non-default test db
-      redis.select(9);
-      redis.flushdb();
+            // Build N concurrent hit queries
+            function handler(i) {
+                return function(callback) {
+                    ts.recordHit('key_' + i).exec(callback)
+                }
+            }
 
-      async.parallel(range(N).map(handler), this.callback);
-    },
+            // Select non-default test db
+            redis.select(9)
+            redis.flushdb()
 
-    "we get correct results": function(results) {
-      redis.flushdb();
+            async.parallel(range(N).map(handler), this.callback)
+        },
 
-      var expected = range(N).map(function() { return [1,1]; });
+        "we get correct results": function(results) {
+            redis.flushdb()
 
-      assert.deepEqual(results, expected);
+            var expected = range(N).map(function() {
+                return [1, 1]
+            })
+
+            assert.deepEqual(results, expected)
+        }
     }
-  }
-}).export(module);
+}).export(module)
